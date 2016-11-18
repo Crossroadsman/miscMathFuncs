2042589286e241f45a3f0dd0d326680b6712cbf1
Alex Koumparos <crossroadsman@gmail.com>
2016-11-18 11:30:17 -0700

    Create binomialCoefficient.swift

---
+++ binomialCoefficient.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Double {
+    func squared() -> Double {
+        return self * self
+    }
+    
+    func factorial() -> Double {
+        if self <= 1 {
+            return self
+        }
+        return self * (self - 1).factorial()
+    }
+
+}
+
+/**
+ WARNING: because this uses factorial(), it becomes computationally infeasible very quickly.
+ 
+ n is the number of items
+ r is the number chosen
+ */
+func binomialCoefficient(n: Double, r: Double) -> Double {
+    let nf = n.factorial()
+    let rf = r.factorial()
+    let nsrf = (n - r).factorial()
+    return nf / (rf * nsrf)
+}
+
+binomialCoefficient(n: 16, r: 3)
+binomialCoefficient(n: 3, r: 4).squared()
